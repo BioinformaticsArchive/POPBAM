c639e1929030fe091719e3eceb960e24294bbc99
Daniel Garrigan <dgarriga@bio.rochester.edu>
2013-07-24 14:35:40 -0700

    Changed the way tables are represented in the header files and deleted the subversion code and modified the Makefile to compile C and C++ files separately

---
+++ tables.h
@@ -0,0 +1,20 @@
+/** \file tables.h
+ *  \brief Header within definitions of tables
+ *  \author Daniel Garrigan
+ *  \version 0.3
+ */
+
+/*! \def bam_nt16_table[256]
+ *  \brief A lookup table
+ */
+ extern unsigned char bam_nt16_table[256];
+
+/*! \def iupac[16]
+ *  \brief A lookup table for IUPAC codes
+ */
+extern const char iupac[16];
+
+/*! \def bam_nt16_nt4_table[16]
+ *  \brief A reverse lookup table
+ */
+extern int bam_nt16_nt4_table[16];
